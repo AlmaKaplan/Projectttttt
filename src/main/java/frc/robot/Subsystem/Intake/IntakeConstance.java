92f03722b74be4fc7bcb0f8f9df572699dcfbecd
AlmaKaplan <alma20062010@gmail.com>
2024-12-09 20:30:46 +0200

    I forgot to do save so Intake IO real again :)

--- src/main/java/frc/robot/Subsystem/Intake/IntakeConstance.java
+++ src/main/java/frc/robot/Subsystem/Intake/IntakeConstance.java
@@ -1,10 +1,25 @@
 
 package frc.robot.Subsystem.Intake;
 
+import frc.robot.Robot;
+import frc.robot.Subsystem.Intake.IOs.IntakeIO;
+import frc.robot.Subsystem.Intake.IOs.IntakeIOReal;
+import frc.robot.Subsystem.Intake.IOs.IntakeIOSim;
+
 public class IntakeConstance {
     public static final double INTAKE_GEAR = 1;
     public static final double PeakCurrentLimit = 30; 
     public static final double ContinuesCurrentLimit = 25; 
     public static final double PeakCurrentTime = 0.1; 
     public static final boolean IsCurrentLimitEnabled = true; 
+
+
+
+    public static final IntakeIO getIntakeIO() {
+        if (Robot.isReal()) {
+            return new IntakeIOReal();
+        } else {
+            return new IntakeIOSim();
+        }
+    }
 }
